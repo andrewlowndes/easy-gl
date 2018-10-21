4673e8ad712b9fdf237e51f422871538dfe6884c
mr.boggieman <mr.boggieman@gmail.com>
2018-10-21 21:52:29 +0100

    Add build files for premultiplyAlpha texture changes

--- lib/WebGL/createTexture.d.ts
+++ lib/WebGL/createTexture.d.ts
@@ -11,5 +11,21 @@ export interface createTextureOptions {
     srcType: number;
     width: number;
     height: number;
+    premultiplyAlpha: boolean;
 }
+export declare const defaultOptions: {
+    minFilter: number;
+    magFilter: number;
+    wrapS: number;
+    wrapT: number;
+    flipY: boolean;
+    level: number;
+    internalFormat: number;
+    border: number;
+    srcFormat: number;
+    srcType: number;
+    width: number;
+    height: number;
+    premultiplyAlpha: boolean;
+};
 export declare function createTexture(gl: WebGLRenderingContext, opts?: Partial<createTextureOptions>): WebGLTexture;
